74cbbe4947e9b4e96a7f0e214aac89233f464608
j.leek <j.leek@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-09-04 01:06:06 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: fc897f2fddb35edb595bb6eb4ab76a290efe2877
    
        added sva.check function
    
    Committed by: jtleek
    Author Name: jtleek
    Commit date: 2014-09-03 21:05:49 -0400
    Author date: 2014-09-03 21:05:49 -0400
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/sva@93788 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/sva.check.R
@@ -0,0 +1,29 @@
+#' A function for post-hoc checking of an sva object to check for
+#' degenerate cases. 
+#' 
+#' This function is designed to check for degenerate cases in the
+#' sva fit and fix the sva object where possible. 
+#' 
+#' \code{\link{empirical.controls}} for a direct estimate of the empirical controls. 
+#' 
+#' @param svaobj The transformed data matrix with the variables in rows and samples in columns
+#' @param dat
+#' @param mod The model matrix being used to fit the data
+#' @param mod0 The null model matrix being used to fit the data
+#' 
+#' @return sv The estimated surrogate variables, one in each column
+#' @return pprob.gam: A vector of the posterior probabilities each gene is affected by heterogeneity
+#' @return pprob.b A vector of the posterior probabilities each gene is affected by mod
+#' @return n.sv The number of significant surrogate variables
+#' 
+#' @export
+#' 
+#' 
+
+sva.check <- function(svaobj,dat,mod,mod0){
+  if(mean(svaobj$pprob.gam) > 0.95){
+    message("Nearly all genes identified as batch associated, estimates of batch may be compromised. Please check your data carefully before using sva. Reverting to 2-step sva algorithm.")
+    svaobj = sva(dat,mod,mod0,method="two-step")
+  }
+  return(svaobj)
+}
