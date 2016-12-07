07d4e90770948c2cf7d073d1e0ba5fb64e91ee79
Ryan C. Thompson <rct@thompsonclan.org>
2016-12-07 14:07:13 -0800

    Make variance prior plots reproducible
    This avoids the use of rgamma to generate the theoretical quantiles for
    the variance plot. Instead, is uses dgamma, qgamma, and ppoints, which
    are all deterministic.

--- R/ComBat.R
+++ R/ComBat.R
@@ -151,17 +151,18 @@ ComBat <- function(dat, batch, mod=NULL, par.prior=TRUE,prior.plots=FALSE,mean.o
     plot(tmp,  type='l', main="Density Plot")
     xx <- seq(min(tmp$x), max(tmp$x), length=100)
     lines(xx,dnorm(xx,gamma.bar[1],sqrt(t2[1])), col=2)
-    qqnorm(gamma.hat[1,])	
-    qqline(gamma.hat[1,], col=2)	
-    
+    qqnorm(gamma.hat[1,])
+    qqline(gamma.hat[1,], col=2)
+
     tmp <- density(delta.hat[1,])
-    invgam <- 1/rgamma(ncol(delta.hat),a.prior[1],b.prior[1])
-    tmp1 <- density(invgam)
-    plot(tmp,  typ='l', main="Density Plot", ylim=c(0,max(tmp$y,tmp1$y)))
+    xx <- seq(min(tmp$x), max(tmp$x), length=100)
+    tmp1 <- list(x=xx, y=dgamma(xx, a.prior[1], b.prior[1]))
+    plot(tmp, typ="l", main="Density Plot", ylim=c(0, max(tmp$y, tmp1$y)))
     lines(tmp1, col=2)
-    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab='Theoretical Quantiles')	
-    lines(c(0,max(invgam)),c(0,max(invgam)),col=2)	
-    title('Q-Q Plot')
+    invgam <- 1/qgamma(ppoints(ncol(delta.hat)), a.prior[1], b.prior[1])
+    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab="Theoretical Quantiles")
+    lines(c(0, max(invgam)), c(0, max(invgam)), col=2)
+    title("Q-Q Plot")
   }
   
   ##Find EB batch adjustments
