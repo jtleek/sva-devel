jtleek/sva-devel

commit 07d4e90770948c2cf7d073d1e0ba5fb64e91ee79

Make variance prior plots reproducible
This avoids the use of rgamma to generate the theoretical quantiles for
the variance plot. Instead, is uses dgamma, qgamma, and ppoints, which
are all deterministic.

R/ComBat.R
@@ -151,17 +151,18 @@ ComBat <- function(dat, batch, mod=NULL, par.prior=TRUE,prior.plots=FALSE,mean.o
     plot(tmp,  type='l', main="Density Plot")
     xx <- seq(min(tmp$x), max(tmp$x), length=100)
     lines(xx,dnorm(xx,gamma.bar[1],sqrt(t2[1])), col=2)
-    qqnorm(gamma.hat[1,])	
-    qqline(gamma.hat[1,], col=2)	
-    
+    qqnorm(gamma.hat[1,])
+    qqline(gamma.hat[1,], col=2)
+
     tmp <- density(delta.hat[1,])
-    invgam <- 1/rgamma(ncol(delta.hat),a.prior[1],b.prior[1])
-    tmp1 <- density(invgam)
-    plot(tmp,  typ='l', main="Density Plot", ylim=c(0,max(tmp$y,tmp1$y)))
+    xx <- seq(min(tmp$x), max(tmp$x), length=100)
+    tmp1 <- list(x=xx, y=dgamma(xx, a.prior[1], b.prior[1]))
+    plot(tmp, typ="l", main="Density Plot", ylim=c(0, max(tmp$y, tmp1$y)))
     lines(tmp1, col=2)
-    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab='Theoretical Quantiles')	
-    lines(c(0,max(invgam)),c(0,max(invgam)),col=2)	
-    title('Q-Q Plot')
+    invgam <- 1/qgamma(ppoints(ncol(delta.hat)), a.prior[1], b.prior[1])
+    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab="Theoretical Quantiles")
+    lines(c(0, max(invgam)), c(0, max(invgam)), col=2)
+    title("Q-Q Plot")
   }
   
   ##Find EB batch adjustments

commit 644679a1821fa08886278158c726a9bc96bd7cfd

Fix issues in ComBat prior plotting code
* Bottom left plot now plots inverse gamma instead of gamma (fixes bug
  introduced in 07d4e907)
* Axes are now consistent between the two Q-Q plots: Y = sample
  quantiles, X = theoretical quantiles (fixes #19)
* Plot titles are now more descriptive

R/ComBat.R
@@ -146,25 +146,34 @@ ComBat <- function (dat, batch, mod = NULL, par.prior = TRUE, prior.plots = FALS
   
   
   ##Plot empirical and parametric priors
-  
-  if (prior.plots & par.prior){
+
+  if (prior.plots && par.prior) {
     par(mfrow=c(2,2))
+
+    # Top left
     tmp <- density(gamma.hat[1,])
-    plot(tmp,  type='l', main="Density Plot")
+    plot(tmp,  type='l', main=expression(paste("Density Plot of First Batch ",  hat(gamma))))
     xx <- seq(min(tmp$x), max(tmp$x), length=100)
     lines(xx,dnorm(xx,gamma.bar[1],sqrt(t2[1])), col=2)
-    qqnorm(gamma.hat[1,])
+
+    # Top Right
+    qqnorm(gamma.hat[1,], main=expression(paste("Normal Q-Q Plot of First Batch ", hat(gamma))))
     qqline(gamma.hat[1,], col=2)
 
+    # Bottom Left
     tmp <- density(delta.hat[1,])
     xx <- seq(min(tmp$x), max(tmp$x), length=100)
-    tmp1 <- list(x=xx, y=dgamma(xx, a.prior[1], b.prior[1]))
-    plot(tmp, typ="l", main="Density Plot", ylim=c(0, max(tmp$y, tmp1$y)))
+    tmp1 <- list(x=xx, y=dinvgamma(xx, a.prior[1], b.prior[1]))
+    plot(tmp, typ="l", ylim=c(0, max(tmp$y, tmp1$y)),
+         main=expression(paste("Density Plot of First Batch ", hat(delta))))
     lines(tmp1, col=2)
-    invgam <- 1/qgamma(ppoints(ncol(delta.hat)), a.prior[1], b.prior[1])
-    qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab="Theoretical Quantiles")
+
+    # Bottom Right
+    invgam <- 1/qgamma(1-ppoints(ncol(delta.hat)), a.prior[1], b.prior[1])
+    qqplot(invgam, delta.hat[1,],
+           main=expression(paste("Inverse Gamma Q-Q Plot of First Batch ", hat(delta))),
+           ylab="Sample Quantiles", xlab="Theoretical Quantiles")
     lines(c(0, max(invgam)), c(0, max(invgam)), col=2)
-    title("Q-Q Plot")
   }
   
   ##Find EB batch adjustments

R/helper.R
@@ -63,6 +63,14 @@ bprior <- function(gamma.hat){m=mean(gamma.hat); s2=var(gamma.hat); (m*s2+m^3)/s
 postmean <- function(g.hat,g.bar,n,d.star,t2){(t2*n*g.hat+d.star*g.bar)/(t2*n+d.star)}
 postvar <- function(sum2,n,a,b){(.5*sum2+b)/(n/2+a-1)}
 
+# Inverse gamma distribution density function. (Note: does not do any bounds checking on arguments)
+dinvgamma <- function (x, shape, rate = 1/scale, scale = 1) {
+    # PDF taken from https://en.wikipedia.org/wiki/Inverse-gamma_distribution
+    # Note: alpha = shape, beta = rate
+    stopifnot(shape > 0)
+    stopifnot(rate > 0)
+    ifelse(x <= 0, 0, ((rate ^ shape) / gamma(shape)) * x ^ (-shape - 1) * exp(-rate/x))
+}
 
 # Pass in entire data set, the design matrix for the entire data, the batch means, the batch variances, priors (m, t2, a, b), columns of the data  matrix for the batch. Uses the EM to find the parametric batch adjustments
 