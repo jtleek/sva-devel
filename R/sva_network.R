309f10812943f340573789977eecbd87dec2fa8b
claireruberman <claireruberman@gmail.com>
2018-07-16 11:01:00 -0400

    Added sva_network() function
    Added sva_network() function to remove top principal components from
    raw gene expression data.

--- R/sva_network.R
+++ R/sva_network.R
@@ -1,18 +1,35 @@
+#' A function to adjust gene expression data before network inference
+#' 
+#' This function corrects a gene expression matrix prior to network inference by
+#' returning the residuals after regressing out the top principal components. The number 
+#' of principal components to remove can be determined using a permutation-based approach
+#' using the "num.sv" function with method = "be"
 
-## sva_network() is a function to adjust expression data before network inference
-## The function inputs raw gene expression data (raw) and the number of principal components to remove (n.pc) and returns a cleaned matrix with those prinicpal components removed.
-## The number of prinicpal components can be determined using the "num.sv" function: n.pc=num.sv(t(dat), mod,method="be")
+#' @param dat The raw gene expression data matrix (with the variables in rows and samples in columns)
+#' @param n.pc The number of principal components to remove 
 
+#' @return dat.adjusted Cleaned gene expression data matrix with the top prinicpal components removed
+#' 
+#' @examples 
+#' library(bladderbatch)
+#' data(bladderdata)
+#' dat <- bladderEset[1:5000,]
+#' 
+#' pheno = pData(dat)
+#' edata = exprs(dat)
+#' mod = model.matrix(~as.factor(cancer), data=pheno)
+#' 
+#' n.pc = num.sv(edata, mod, method="be")
+#' dat.adjusted = sva_network(edata, n.pc)
+#' 
+#' @export
+#' 
 sva_network=function(dat, n.pc){
-
-	## singular value decomposition
 	ss<-svd(dat - colMeans(dat))
 
-	## use residuals from top n.pc principal components
 	dat.adjusted=dat
 	for (i in 1:dim(dat)[2]){
-  		dat.adjusted[,i]<- lm(dat[,i] ~ ref.ss$u[,1:n.pc])$residuals
+  		dat.adjusted[,i]<- lm(dat[,i] ~ ss$u[,1:n.pc])$residuals
 	}
 	return(dat.adjusted)
-
 }
