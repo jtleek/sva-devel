9ef2e4598687460beabe388d45669dcac1388278
Princy Parsana <princyp89@gmail.com>
2020-03-17 16:15:34 -0400

    update documentation, rows are samples and columns are genes in the dat matrix input for sva_network; input data is uncorrected normalized gene expression data; update example code accordingly

--- R/sva_network.R
+++ R/sva_network.R
@@ -5,7 +5,7 @@
 #' The number of principal components to remove can be determined using a 
 #' permutation-based approach using the "num.sv" function with method = "be"
 
-#' @param dat The raw gene expression data matrix (with variables in rows and samples in columns)
+#' @param dat The uncorrected normalized gene expression data matrix with samples in rows and genes in columns
 #' @param n.pc The number of principal components to remove 
 
 #' @return dat.adjusted Cleaned gene expression data matrix with the top prinicpal components removed
@@ -15,12 +15,11 @@
 #' data(bladderdata)
 #' dat <- bladderEset[1:5000,]
 #' 
-#' pheno = pData(dat)
 #' edata = exprs(dat)
-#' mod = model.matrix(~as.factor(cancer), data=pheno)
+#' mod = matrix(1, nrow = dim(dat)[2], ncol = 1)
 #' 
 #' n.pc = num.sv(edata, mod, method="be")
-#' dat.adjusted = sva_network(edata, n.pc)
+#' dat.adjusted = sva_network(t(edata), n.pc)
 #' 
 #' @export
 #' 
